db7271cfd4f4011651c57da1edd8801214e3736a
Andrew Mitchell <mitche50@gmail.com>
2019-10-28 22:53:35 -0400

    decode before json convert

--- dpow.py
+++ dpow.py
@@ -154,19 +154,19 @@ def index():
     redis_day_data, redis_day_precache, redis_day_ondemand = [], [], []
 
     for i in range(0, r.llen("minute_data")):
-        redis_minute_data.append(json.loads(r.lindex("minute_data", i)))
-        redis_minute_ondemand.append(json.loads(r.lindex("minute_ondemand", i)))
-        redis_minute_precache.append(json.loads(r.lindex("minute_precache", i)))
+        redis_minute_data.append(json.loads(r.lindex("minute_data", i).decode('utf-8')))
+        redis_minute_ondemand.append(json.loads(r.lindex("minute_ondemand", i).decode('utf-8')))
+        redis_minute_precache.append(json.loads(r.lindex("minute_precache", i).decode('utf-8')))
     
     for i in range(0, r.llen("day_data")):
-        redis_day_data.append(json.loads(r.lindex("day_data", i)))
-        redis_day_ondemand.append(json.loads(r.lindex("day_ondemand", i)))
-        redis_day_precache.append(json.loads(r.lindex("day_precache", i)))
+        redis_day_data.append(json.loads(r.lindex("day_data", i).decode('utf-8')))
+        redis_day_ondemand.append(json.loads(r.lindex("day_ondemand", i).decode('utf-8')))
+        redis_day_precache.append(json.loads(r.lindex("day_precache", i).decode('utf-8')))
     
     for i in range(0, r.llen("hour_data")):
-        redis_hour_data.append(json.loads(r.lindex("hour_data", i)))
-        redis_hour_ondemand.append(json.loads(r.lindex("hour_ondemand", i)))
-        redis_hour_precache.append(json.loads(r.lindex("hour_precache", i)))
+        redis_hour_data.append(json.loads(r.lindex("hour_data", i).decode('utf-8')))
+        redis_hour_ondemand.append(json.loads(r.lindex("hour_ondemand", i).decode('utf-8')))
+        redis_hour_precache.append(json.loads(r.lindex("hour_precache", i).decode('utf-8')))
 
     avg_combined_time = db.get_db_data(avg_combined_call)
     avg_overall_data = db.get_db_data(avg_overall_call)
